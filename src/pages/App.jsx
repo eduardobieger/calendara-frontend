cbdbf6e2be2fd447f714f8719b04b56ebf308ffe
Eduardo <eduardo42bieger@gmail.com>
2024-02-25 07:34:37 -0300

    moved file location to pages folder

---
+++ src/pages/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Calendara</h1>
+      <p>Welcome to Calendara, a web app to schedule your things.</p>
+    </>
+  );
+}
+
+export default App;
