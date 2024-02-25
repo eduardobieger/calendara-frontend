ea636ce7f4fbbb3fa963b3c68198a36136b2bc35
Eduardo <eduardo42bieger@gmail.com>
2024-02-25 08:23:24 -0300

    added login route

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./pages/App.jsx";
+import App from "./pages/App";
+import Login from "./pages/Login";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
