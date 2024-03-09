461dc458e7aaf81a58dbc755e8563f6f63d062ca
Eduardo <eduardo42bieger@gmail.com>
2024-03-09 17:39:51 -0300

    added routes to new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Pricing from "./pages/Pricing";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 }
