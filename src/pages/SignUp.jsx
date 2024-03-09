457c9afb72bee5871469d86792a9e23c100bfbe1
Eduardo <eduardo42bieger@gmail.com>
2024-03-09 15:34:46 -0300

    created sign up page to create new user

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,34 @@
+import Navbar from "../components/Navbar";
+
+export default function SignUp() {
+  return (
+    <>
+      <Navbar />
+      <div className="m-auto max-w-96 flex items-center justify-center my-60">
+        <form className="bg-zinc-800 p-6 rounded shadow-md w-full">
+          <div className="flex flex-col mb-4">
+            <label htmlFor="email" className="mb-2">
+              Email
+            </label>
+            <input type="email" id="email" className="border p-1 rounded" />
+          </div>
+          <div className="flex flex-col mb-4">
+            <label htmlFor="username" className="mb-2">
+              Username
+            </label>
+            <input type="text" id="username" className="border p-1 rounded" />
+          </div>
+          <div className="flex flex-col mb-4">
+            <label htmlFor="password" className="mb-2">
+              Password
+            </label>
+            <input type="password" id="password" className="border p-1 rounded" />
+          </div>
+          <button type="submit" className="bg-blue-800 p-3 rounded w-full hover:bg-blue-600">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
