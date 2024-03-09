f6f106bd0e28497f9cf0efd345217e5c353ab258
Eduardo <eduardo42bieger@gmail.com>
2024-03-09 13:43:21 -0300

    reconfigured routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./pages/App";
-import Login from "./pages/Login";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
