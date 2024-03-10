486dc674853bf109f0b0da349e2be25bca45d007
Eduardo <eduardo42bieger@gmail.com>
2024-03-09 21:21:09 -0300

    aligned text to center

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col justify-center items-center my-20">
+      <div className="flex flex-col justify-center items-center text-center my-20">
         <h1 className="text-5xl">Your scheduling automation platform</h1>
-        <p className="text-xl mt-10 max-w-xl text-center leading-8">Your go-to app for effortlessly scheduling appointments, featuring intuitive automation to streamline your day and optimize your agenda smartly.</p>
+        <p className="text-xl mt-10 max-w-xl leading-8">Your go-to app for effortlessly scheduling appointments, featuring intuitive automation to streamline your day and optimize your agenda smartly.</p>
       </div>
       <Footer />
     </>
